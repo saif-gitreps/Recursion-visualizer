1f996ba0faad41463315badaf01608c462bfa19b
Saif <droconos99@gmail.com>
2023-09-30 14:12:28 +0600

    implementing stack

--- app.js
+++ app.js
@@ -8,6 +8,25 @@ const firstBlockElement = document.createElement("div");
 let rowValue = rowInput.value;
 let colValue = colInput.value;
 
+class Stack {
+   constructor() {
+      this.stack = [];
+   }
+   push(item) {
+      this.stack.push(item);
+   }
+   pop() {
+      if (this.stack.length == 0) return;
+      this.stack.pop();
+   }
+   top() {
+      return this.stack[this.stack.length - 1];
+   }
+   empty() {
+      return this.stack.length == 0;
+   }
+}
+
 function reduceWidth(width) {
    if (width > 45 && width < 60) {
       width = width - width * (1 / 3);
@@ -49,6 +68,7 @@ enterButton.addEventListener("click", () => {
    initialBlock();
    simulateButton.style.display = "block";
 });
+
 function kev(m, n, row, col) {
    if (m == row && n == col) {
       return;
@@ -56,47 +76,23 @@ function kev(m, n, row, col) {
    if (m < row) {
       m += 1;
       console.log("right");
-      const blockElement = document.createElement("div");
-      blockElement.style.gridRowStart = m;
-      blockElement.style.gridColumnStart = n;
-      blockElement.classList.add("block");
-      grid.appendChild(blockElement);
-
-      setTimeout(() => {
-         kev(m, n, row, col);
-         setTimeout(() => {
-            grid.removeChild(blockElement);
-            console.log("left");
-            m -= 1;
-         }, 800);
-      }, 100);
+      kev(m, n, row, col);
+      console.log("left");
+      m -= 1;
    }
    if (n < col) {
-      console.log("down");
       n += 1;
-      const blockElement = document.createElement("div");
-      blockElement.style.gridRowStart = m;
-      blockElement.style.gridColumnStart = n;
-      blockElement.classList.add("block");
-      grid.appendChild(blockElement);
-
-      setTimeout(() => {
-         kev(m, n, row, col);
-         setTimeout(() => {
-            grid.removeChild(blockElement);
-            console.log("up");
-            n -= 1;
-         }, 800);
-      }, 1400);
+      console.log("down");
+      kev(m, n, row, col);
+      console.log("up");
+      n -= 1;
    }
 }
-
-// IDEA 2 : IS THAT EVERYTIME IT GOES RIGHT , I SEND A "RIGHT IN ANOTHER FUNCTION"
-// ANYTIME IT GOES LEFT , ISEND LEFT IN ANOTHER FUNCTION .
 
 simulateButton.addEventListener("click", () => {
    grid.removeChild(firstBlockElement);
    kev(1, 1, rowValue, colValue);
 });
 
-//PERFECT TIME1 =  UP:{800(INNER CLOCK)],100}  {DOWN:800(INNER CLOCK),1400}.
+//PERFECT TIME1 =  UP:{800(INNER CLOCK),100}  {DOWN:800(INNER CLOCK),1400}.
+//PERFECT TIME1 =  UP:{1000(INNER CLOCK),200}  {DOWN:1000(INNER CLOCK),200}.
